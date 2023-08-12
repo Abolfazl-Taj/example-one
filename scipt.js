a9ec900cebe7bcf92adfa67d67624cd27de64f19
Abolfazl Taj <103893533+Abolfazl-Taj@users.noreply.github.com>
2023-08-12 15:29:50 +0330

    Add files via upload

---
+++ scipt.js
@@ -0,0 +1,13 @@
+let borderaval = document.querySelector(".four");
+let borderdovom = document.querySelector(".five");
+let bordersevom = document.querySelector(".six");
+borderaval.addEventListener("click" ,() =>{
+    borderaval.classList.toggle("input-border")
+})
+borderdovom.addEventListener("click" ,() =>{
+    borderdovom.classList.toggle("input-border")
+
+   })
+   bordersevom.addEventListener("click" ,() =>{
+    bordersevom.classList.toggle("input-border")
+   })
